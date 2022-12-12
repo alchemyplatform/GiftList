14e730fd69becb6d826e6549f74cc507662f4a35
Fazal E Rabbi <fazalerabbi@Fazals-MacBook-Pro.local>
2022-12-12 13:10:16 +0500

    Create the merkle tree, proof and verify

--- client/index.js
+++ client/index.js
@@ -4,11 +4,23 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const index = niceList.findIndex(n => n === process.argv[2]);
+if (!index || index < 0 || index > niceList.length || !process.argv[2]) {
+  console.log('Name not found in nice list!');
+  process.exit(1);
+}
+
+const leaf = process.argv[3];
+const merkleTree = new MerkleTree(niceList);
+const proof = merkleTree.getProof(index);
+
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    leaf,
+    proof,
   });
 
   console.log({ gift });
