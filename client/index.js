5270dfabdc48cf522d78ce2768252abeb6844ab5
Sheikh Jamir Alam <alamjamir15@gmail.com>
2023-06-22 22:30:36 +0530

    task comeplete

--- client/index.js
+++ client/index.js
@@ -1,14 +1,21 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = "Mr. Otis Koelpin III";
+  const merkleTree = new MerkleTree(niceList);
+  console.log(typeof merkleTree.getRoot());
+  const index = niceList.indexOf(name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    leaf: name,
+    proof,
   });
 
   console.log({ gift });
--- utils/example.js
+++ utils/example.js
@@ -1,6 +1,6 @@
-const MerkleTree = require('./MerkleTree');
-const niceList = require('./niceList');
-const verifyProof = require('./verifyProof');
+const MerkleTree = require("./MerkleTree");
+const niceList = require("./niceList");
+const verifyProof = require("./verifyProof");
 
 // create the merkle tree for the whole nice list
 const merkleTree = new MerkleTree(niceList);
@@ -8,12 +8,12 @@ const merkleTree = new MerkleTree(niceList);
 // get the root
 const root = merkleTree.getRoot();
 
-// find the proof that norman block is in the list 
-const name = 'Norman Block';
-const index = niceList.findIndex(n => n === name);
+// find the proof that norman block is in the list
+const name = "Norman Block";
+const index = niceList.findIndex((n) => n === name);
 const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
-console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+console.log(verifyProof(proof, name, root)); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
